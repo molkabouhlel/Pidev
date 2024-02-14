9f95b5c05075379ac3983490df532c8b4ad1745b
dorra <dorra.benarbi@esprit.tn>
2024-02-14 18:42:50 +0100

    testcrud

---
+++ src/Interfaces/Interface.java
@@ -0,0 +1,8 @@
+package Interfaces;
+import java.util.List;
+public interface Interface <T> {
+    public void add(T t);
+    public void update(T t);
+    public void supp( T t);
+    public List<T> show();
+}
---
+++ src/Models/Admin.java
@@ -0,0 +1,6 @@
+package Models;
+
+public class Admin  extends User{
+
+
+}
