ed97604422da2b150c8e304e7fc128ae343a7f41
molka <molka.bouhlel@esprit.tn>
2024-02-22 00:01:41 +0100

    Major Update

--- molka/src/Interfaces/Interface.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-import java.util.List;
-public interface Interface <T> {
-    public void add(T t);
-    public void update(T t);
-    public void supp( T t);
-    public List<T> show();
-}
